e798e73174b5a3f2afd7a187092bc3a234b48520
Pranav <sinhapranavraj10142@gmail.com>
2023-06-07 15:04:32 +0530

    header build

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* Header */}
+      <Header/>
+      {/* Tinder Cards */}
+      {/* Swipe buttons  */}
+    </div>
+  );
+}
+
+export default App;
