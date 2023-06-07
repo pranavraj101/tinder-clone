35bc5ea6cad92e7660ef4bb574cfa3435ba0ddbb
Pranav <sinhapranavraj10142@gmail.com>
2023-06-07 17:37:00 +0530

    modiffied header

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import "./header.css"
 import Person4Icon from '@mui/icons-material/Person4';
-import IconButton from '@mui/material/IconButton';
 import ForumIcon from '@mui/icons-material/Forum';
 
 
 function Header(){
     return <div className="header">
-        <IconButton>
             <Person4Icon fontSize ="large" className="header_icon"/>
-        </IconButton>
+
 
         <img className='header_logo' src="https://static.vecteezy.com/system/resources/previews/021/460/424/original/tinder-logo-transparent-free-png.png"></img>
 
-        <IconButton>
+
             <ForumIcon fontSize='large' className='header_icon'/>
-        </IconButton>
+
     </div>
 }
 
